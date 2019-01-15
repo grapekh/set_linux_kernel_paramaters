f2bbeb3be4259ed8a258e7752d452d2de377b793
Howie Grapek <41378306+grapekh@users.noreply.github.com>
2019-01-15 17:37:22 -0500

    Create set_params.sh

---
+++ set_params.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#
+# Parameters needed for Palms
+#
+# Grapek - 20190115
+#
+# run this as root!
+#
+
+echo "Here are the current limits..."
+ipcs -l
+
+echo ""
+echo "Increasing memory limits..."
+sysctl -w kernel.msgmax=65536
+sysctl -w kernel.msgmnb=65536
+sysctl -w kernel.shmmax=18014398509465599
+sysctl -w kernel.shmall=18014398442373116
+
+
+echo ""
+echo "Increasing memory limits permanently..."
+
+echo "# 
+# sysctl configuration file for Red Hat Linux - for PALMS
+#
+# Changes needed for Palms...
+#
+kernel.msgmnb=65536
+kernel.msgmax=65536
+kernel.shmmax=18014398509465599
+kernel.shmall=18014398442373116" > /etc/sysctl.d/01-custom.conf
+
+
+
+echo ""
+echo "Setting up semaphores in file... "
+echo "kernel.sem = 65535 512000 1024 1024" >> /etc/sysctl.conf 
+
+echo ""
+echo "Testing semaphores and making them so... "
+sysctl -p
+
+echo ""
+echo "Making everything permanent..."
+sysctl --system
+
+
+echo ""
+echo "All done - here are the settings"
+echo ""
+ipcs -l
